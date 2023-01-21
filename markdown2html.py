087d59710cd0eb47a6bdadb10810ab934dc6052a
melakualehegn <melakualehegn34@gmail.com>
2023-01-21 23:00:38 +0300

    markdownfix

--- markdown2html.py
+++ markdown2html.py
@@ -4,12 +4,10 @@
 import sys
 from os import path
 
-
-if __name__ == "__main__":
-    if (len(sys.argv) < 3):
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
-        sys.exit(1)
-    elif not path.exists(sys.argv[1]):
-        sys.stderr.write("Missing {}".format(sys.argv[1]))
-        sys.exit(1)
-    sys.exit(0)
+if (len(sys.argv) < 2):
+    sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
+    sys.exit(1)
+elif not path.exists(sys.argv[1]):
+    sys.stderr.write("Missing {}".format(sys.argv[1]))
+    sys.exit(1)
+sys.exit(0)
