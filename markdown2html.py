de345ad36e92d0747113f11e6174577a25c35492
melakualehegn <melakualehegn34@gmail.com>
2023-01-21 22:20:30 +0300

    Imort error

--- markdown2html.py
+++ markdown2html.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-import markdown
 import sys
 from os import path
-
-if (len(sys.argv) < 1):
-    sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
-    sys.exit(1)
-elif not path.exists(sys.argv[0]):
-    sys.stderr.write("Missing {}".format(sys.argv[1]))
-    sys.exit(1)
-sys.exit(0)
+if __name__ == "__main__":
+    if (len(sys.argv) < 1):
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
+        sys.exit(1)
+    elif not path.exists(sys.argv[0]):
+        sys.stderr.write("Missing {}".format(sys.argv[1]))
+        sys.exit(1)
+    sys.exit(0)
