017fa5dafd15187cad324b695ab8ca671b17f8fb
melakualehegn <melakualehegn34@gmail.com>
2023-01-21 22:23:03 +0300

    Documentation

--- markdown2html.py
+++ markdown2html.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""A script that checks all the requirements"""
 import sys
 from os import path
+
+
 if __name__ == "__main__":
     if (len(sys.argv) < 1):
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
