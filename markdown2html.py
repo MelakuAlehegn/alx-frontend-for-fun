45586ab9463dc67df9f61ea0efed52326b68dfe3
melakualehegn <melakualehegn34@gmail.com>
2023-01-21 22:33:10 +0300

    Documentation

--- markdown2html.py
+++ markdown2html.py
@@ -5,10 +5,10 @@ from os import path
 
 
 if __name__ == "__main__":
-    if (len(sys.argv) < 2):
+    if (len(sys.argv) < 3):
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
         sys.exit(1)
-    elif not path.exists(sys.argv[0]):
+    elif not path.exists(sys.argv[1]):
         sys.stderr.write("Missing {}".format(sys.argv[1]))
         sys.exit(1)
     sys.exit(0)
